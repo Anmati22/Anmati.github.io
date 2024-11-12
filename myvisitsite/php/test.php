3e5ff7b120a3f70acb90cc4da38710f2d40d9259
Anfourmati <95656709+Anfourmati@users.noreply.github.com>
2024-11-12 22:27:24 +0300

    Delete myvisitsite directory

--- myvisitsite/php/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$text = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
-$message = $_POST['user_message'];
-echo "Скрипт сработал! <br>". $text . "<br>". $phone . "<br>". $email . "<br>". $message;
-?>
